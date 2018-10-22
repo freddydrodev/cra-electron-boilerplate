9d7ed13f921b62c8e4c885574a38d1a40e34659d
Fredius Tout Court <frediustcdev@gmail.com>
2018-10-22 19:30:47 +0000

    dependencies added for dev mode in electron

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div>Home</div>;
+    return (
+      <div>
+        Home
+        <a href="https://pouchdb.com" target="_blank">
+          PouchDB targeted
+        </a>
+      </div>
+    );
   }
 }
 
